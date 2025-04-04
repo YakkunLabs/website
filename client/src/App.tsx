935bdf1f0e5329dd56c4524e26d5ae048950788f
santhushii <santhushie@gmail.com>
2025-04-04 19:48:56 +0530

    Create store page with product listing and animations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Services from './pages/Services';
-import Industries from './pages/Industries';
-import Company from './pages/Company';
-import Resources from './pages/Resources';
-import Careers from './pages/Careers';
-import Support from './pages/Support';
-import './styles/App.css';
+
+import Store from './pages/Store';
+
 
 function App() {
   return (
@@ -15,12 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/industries" element={<Industries />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/support" element={<Support />} />
+        
+        <Route path="/store" element={<Store />} />
       </Routes>
     </Router>
   );
--- client/src/pages/Store.tsx
+++ client/src/pages/Store.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import './Store.css';
 import nft_01 from '../assets/nft_01.png';
 import nft_02 from '../assets/nft_02.png';
 import nft_03 from '../assets/nft_03.png';
 import nft_04 from '../assets/nft_04.png';
 import nft_05 from '../assets/nft_05.png';
 import nft_06 from '../assets/nft_06.png';
+import '../styles/Store.css';  // Update the import path
 
 const products = [
-  { image: nft_01, title: "NFT Item 1", price: "$50", discount: "10%" },
-  { image: nft_02, title: "NFT Item 2", price: "$75", discount: "15%" },
-  { image: nft_03, title: "NFT Item 3", price: "$100", discount: "5%" },
-  { image: nft_04, title: "NFT Item 4", price: "$200", discount: "20%" },
-  { image: nft_05, title: "NFT Item 5", price: "$150", discount: "12%" },
-  { image: nft_06, title: "NFT Item 6", price: "$80", discount: "8%" },
+  { image: nft_01, title: 'NFT Item 1', price: '$50', discount: '10%' },
+  { image: nft_02, title: 'NFT Item 2', price: '$75', discount: '15%' },
+  { image: nft_03, title: 'NFT Item 3', price: '$100', discount: '5%' },
+  { image: nft_04, title: 'NFT Item 4', price: '$200', discount: '20%' },
+  { image: nft_05, title: 'NFT Item 5', price: '$150', discount: '12%' },
+  { image: nft_06, title: 'NFT Item 6', price: '$80', discount: '8%' },
 ];
 
 const Store = () => {
